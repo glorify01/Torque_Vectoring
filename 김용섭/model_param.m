cff2b9d23f0034731902237eb9ed8468c6480452
glorify01 <dydtjq9480@gmail.com>
2025-03-18 13:54:31 +0900

    add: model param, linear mode

---
+++ 김용섭/model_param.m
@@ -0,0 +1,25 @@
+%% Dissertacao 기반 model param
+% vehicle param
+Vx0 = 50;
+Cyf = 15714;
+Cyr = 21429;
+Izz = 120;
+m = 356;
+lf = 0.873;
+lr = 0.717;
+
+% torque distribution param
+Gr = 4.4;
+tr = 0.65;
+Rw = 0.265;
+
+% state space matrix
+A = [-(Cyf + Cyr)/(m*Vx0) (-lf*Cyf + lr*Cyr)/(m*Vx0) - Vx0;
+    (-lf*Cyf + lr*Cyr)/(Izz*Vx0) -(lf^2*Cyf + lr^2*Cyr)/(Izz*Vx0)];
+
+B = [Cyf/(m*V   x0) 0; 
+    lf*Cyf/Izz (2*tr*Gr)/(Rw*Izz)];
+
+C = eye(2);
+
+D = zeros(2,2);
